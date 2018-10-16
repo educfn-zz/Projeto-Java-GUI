fee2ad0ca86a56f69edf4e7be9596f90e636130e
Eduardo Cardoso Fernandes Neto <eduardo.cardoso10@gmail.com>
2018-10-16 19:55:39 -0300

    v0.0.3 - Janela Fecha
    Agora quando botão x (Close) for pressionado a Janela ira fechar e o programa também, dado que por padrão um JFrame somente se "esconde" quando recebe um close.

--- Front-End/src/frontEnd/Janela.java
+++ Front-End/src/frontEnd/Janela.java
@@ -14,13 +14,17 @@ public class Janela {
     public Janela()
     {
         JFrame jf = new JFrame();
-        jf.setTitle("Título da Janela");
+        jf.setTitle("Matriz");
         jf.setSize(500,400);
         jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Informa que a janela deve 
+        //fechar ao clicar em "close"(botão x)
     }
     
     public static void main (String [] args)
     {
         new Janela();
+        
+        
     }
 }
